f31dba319191fab9a43592f60b9efb85921e2276
1397648351 <wuzedc@163.com>
2018-05-09 22:28:05 +0800

    1

---
+++ Exercise/Voice.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Exercise
+{
+    partial class Voice
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.txt_msg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(12, 51);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 0;
+            this.btn_ok.Text = "OK";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // txt_msg
+            // 
+            this.txt_msg.Location = new System.Drawing.Point(12, 12);
+            this.txt_msg.Name = "txt_msg";
+            this.txt_msg.Size = new System.Drawing.Size(176, 21);
+            this.txt_msg.TabIndex = 1;
+            // 
+            // Voice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txt_msg);
+            this.Controls.Add(this.btn_ok);
+            this.Name = "Voice";
+            this.Text = "Voice";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.TextBox txt_msg;
+    }
+}
---
+++ Exercise/Voice.cs
@@ -0,0 +1,71 @@
+﻿/************************************************
+ * 项目名称 ：Exercise   
+ * 项目描述 ：
+ * 文件名称 ：Voice.cs
+ * 版 本 号 ：v1.0.0.0  
+ * 说    明 ：
+ * 作    者 ：MyPC 
+ * 创建时间 ：2018/5/9 21:15:49 
+ * 更新时间 ：2018/5/9 21:15:49 
+*************************************************/
+
+using System;
+using System.Windows.Forms;
+using SpeechLib;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Exercise
+{
+    public partial class Voice : Form
+    {
+        private SpeechSynthesizer synth;                                                   // Synthesis对象
+        public Voice()
+        {
+            InitializeComponent();
+            this.Load += new EventHandler(Voice_Load);                                     // 窗口加载事件
+            this.Shown += new EventHandler(Voice_Shown);                                   // 窗口显示事件
+            this.FormClosing += new FormClosingEventHandler(Voice_FormClosing);            // 窗口关闭事件
+        }
+
+        // 异步合成语音
+        private void Speak(string text)
+        {
+            synth.SpeakAsync(text);
+        }
+
+        // 窗口加载事件
+        private void Voice_Load(object sender, EventArgs e)
+        {
+            synth = new SpeechSynthesizer();
+            synth.Rate = -1;                                                                // 语速-10到10
+            synth.Volume = 100;                                                             // 音量0到100
+        }
+
+        // 窗口显示事件
+        private void Voice_Shown(object sender, EventArgs e)
+        {
+            Speak("请输入文字");
+        }
+
+        // 窗口关闭事件
+        private void Voice_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            synth.SpeakAsyncCancelAll();                                                    // 停止合成
+            synth.Dispose();                                                                // 释放资源
+        }
+
+        // 按钮点击事件
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            string text = txt_msg.Text;
+            if (string.IsNullOrEmpty(text))
+            {
+                Speak("请输入文字");
+                return;
+            }
+            Speak(txt_msg.Text);
+        }
+    }
+}
